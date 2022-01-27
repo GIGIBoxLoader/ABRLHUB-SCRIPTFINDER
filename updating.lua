40b46d2cca36dd3d825bb0105bee3bf4d73f54a8
GIGIBoxLoader <96883948+GIGIBoxLoader@users.noreply.github.com>
2022-01-27 20:56:03 +0100

    Update updating.lua

--- updating.lua
+++ updating.lua
@@ -2,3 +2,6 @@ local A_2 = game.PlaceId
 local A_1 = game.JobId
 
 game.Players.LocalPlayer:Kick("Unable to find server, OMV. Error code: 2913 \n\nScript service down or not avaliable. Please contact to michi2200_#1674")
+--game.Players.LocalPlayer:Kick("Unable to find the bypasser. Error code: 271 \n\nBypasser is not online or not avaliable. Please contact to michi2200_#1674")
+--game.Players.LocalPlayer:Kick("Update is released. Waiting to taking action to access to the script to everyone. \n\nError code: 273")
+--game.Players.LocalPlayer:Kick("Data base is down. We will fix it. \n\nError code: 274")
