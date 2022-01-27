dd33c6cbd8a75a9a26475aad5b7b46c770e9654a
GIGIBoxLoader <96883948+GIGIBoxLoader@users.noreply.github.com>
2022-01-27 20:46:22 +0100

    Update updating.lua

--- updating.lua
+++ updating.lua
@@ -1,4 +1,4 @@
 local A_2 = game.PlaceId
 local A_1 = game.JobId
 
-game.Players.LocalPlayer:Kick("Unable to find server, OMV. Error code: 2913 \n\n",A_2,A_1"");
+game.Players.LocalPlayer:Kick("Unable to find server, OMV. Error code: 2913 \n\nScript service down or not avaliable. Please contact to michi2200_#1674")
